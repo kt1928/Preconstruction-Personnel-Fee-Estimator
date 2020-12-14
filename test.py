8260e792961c261ee84ae1f1cfca5da008b8c1c1
kt1928 <75771542+kt1928@users.noreply.github.com>
2020-12-13 20:12:11 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
 import computing_final_project as f
 import unittest
+
+class TestRoster(unittest.TestCase):
+    weeks = 4
+
+    title = 'Exec'
+    effort = 50
+    wage = 45
+
+    title2 = 'Employee'
+    effort2 = 100
+    wage2 = 21
