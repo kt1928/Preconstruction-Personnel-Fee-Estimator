12e690e495b2534c8f35ce9f485269e2a798dbac
kt1928 <75771542+kt1928@users.noreply.github.com>
2020-12-13 20:07:51 -0500

    Update source.py

--- source.py
+++ source.py
@@ -5,3 +5,8 @@ class Person:
   jobTitle = ''
   wage = 0
   effort = 0
+
+  def __init__(self, jobTitle, wage, effort):
+      self.jobTitle = jobTitle
+      self.wage = wage
+      self.effort = effort
