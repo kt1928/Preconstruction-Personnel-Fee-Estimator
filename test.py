899a0e9906487617bcb32b819828c5090d6d2b3f
kt1928 <75771542+kt1928@users.noreply.github.com>
2020-12-13 20:12:32 -0500

    Update test.py

--- test.py
+++ test.py
@@ -21,3 +21,15 @@ class TestRoster(unittest.TestCase):
         effort = 100
         person = f.Person(title, wage, effort)
         self.assertIsNotNone(person)
+
+    def test_calc_hours(self):
+        """
+        Tests whether the hours are calculated properly
+        """
+        wage = 45
+
+        person = f.Person(self.title, wage, self.effort)
+        self.assertEqual(person.calc_hours(self.weeks, self.effort), 80)
+
+        person2 = f.Person(self.title2, wage, self.effort2)
+        self.assertEqual(person2.calc_hours(self.weeks, self.effort2), 160)
