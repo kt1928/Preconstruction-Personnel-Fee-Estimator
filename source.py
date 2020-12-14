be117a1ade01e5daac5eb29bc45dcce9a7d55e5c
kt1928 <75771542+kt1928@users.noreply.github.com>
2020-12-13 20:09:20 -0500

    Update source.py

--- source.py
+++ source.py
@@ -14,3 +14,6 @@ class Person:
   def add_personnel(self): # A list for storing Personnel added to the job
       full_roster.append(self)
       print(self.jobTitle + " was added to the roster")
+
+  def calc_hours(self,weeks,effort): # A list to calculate how many hours the personnel will work based on their effort precentage
+      return weeks*40*(effort/100) # Assumed an average work week is 40  hours. Can be ajusted for more accurate estimation
